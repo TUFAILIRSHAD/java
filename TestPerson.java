0450797596db181716ef615ba178276cab61069f
TUFAILIRSHAD <96675035+TUFAILIRSHAD@users.noreply.github.com>
2022-06-30 23:11:52 +0530

    Delete TestPerson.java

--- TestPerson.java
+++
@@ -1,9 +0,0 @@
-public class TestPerson {
-    public static void main(String []args){
-        Person p1= new Person("Ahmad",20);
-        Person p2= new Person("Ahmad",20);
-        System.out.println(p1.toString());
-        System.out.println(p1.hashCode());
-        System.out.println(p1.equals(p2));
-    }
-}
